1478b0cb64fbfd2a49bafa1a9d362ebaf8f20ddf
Pizzosta <pizzostor@gmail.com>
2023-05-15 05:42:13 -0700

    user model

--- 0x01-challenge/user.py
+++ 0x01-challenge/user.py
@@ -1,30 +1,30 @@
 #!/usr/bin/python3
-""" 
-User class
+"""
+User class Module
 """
 
+
 class User():
-    """ Documentation """
+    """ User class  """
 
     def __init__(self):
-        """ Documentation """
+        """ Initializes a new instance of the User class """
         self.__email = None
+
+    @property
+    def email(self):
+        """ Getter method for the email address """
+        return self.__email
 
     @email.setter
     def email(self, value):
-        """ Documentation """
+        """ Setter method for the email address """
         if type(value) is not str:
             raise TypeError("email must be a string")
         self.__email = value
 
-    @property
-    def email(self):
-        """ Documentation """
-        return self.__email
-   
-    
-if __name__ == "__main__":
 
+if __name__ == "__main__":
     u = User()
     u.email = "john@snow.com"
     print(u.email)
